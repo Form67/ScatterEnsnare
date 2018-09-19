741794bc8d01b9ac32a2eadcd97a5bdf3f3e874d
Sam Gould <drmcman67@yahoo.com>
2018-09-19 11:44:31 -0400

    Added update and how ensnare interacts- further work required

--- assets/data/Ensnare.hx
+++ assets/data/Ensnare.hx
@@ -2,12 +2,30 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxGame;
+import flixel.FlxSprite;
+import flixel.graphics.FlxGraphic;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.system.FlxAsset;
 
-class Ensnare extends Sprite
+class Ensnare extends FlxSprite
 {
-	public function new()
+	var _snare:Ensnare;
+	var _grpSnare:FlxTypedGroup<Ensnare>;
+
+	public function new(?X:Float = 0, ?Y:Float = 0)
 	{
-		super();
+		super(X, Y);
+		makeGraphic(16, 16, FlxColor.BLUE);
+		_grpSnare = new FlxTypedGroup<Ensnare>();
+		add(_grpSnare);
+		width = 50;
+		height = 50;
+	}
+	public function collide():Void{
 		
+	}
+	override public function update(elapsed:Float):Void{
+		collide();
+		super.update(elapsed);
 	}
 }
