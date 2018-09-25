0ea988d7a2fc1ecbe269c670ef0771b5df05a857
Xuan Liu <seamasliu@gmail.com>
2018-09-25 18:27:04 -0400

    Enemy class is now structured

--- source/Enemy.hx
+++ source/Enemy.hx
@@ -1,34 +1,87 @@
 package;
 
- import flixel.FlxSprite;
- import flixel.system.FlxAssets.FlxGraphicAsset;
- import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.FlxG;
 import flixel.group.FlxGroup.FlxTypedGroup;
- class Enemy extends FlxSprite
- {
-	 public var speed:Float = 200;
-	 
-     public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset)
-     {
-         super(X, Y);
-		 makeGraphic(32, 32, 0xFFFFFFFF);
-		 
-		 updateHitbox();
-		 
-     }
-	 //Code used from HaxeFlixel documentation
-	 function movement():Void
-	 {
-		var _ypspeed:Float = 0;
-		var _xspeed:Float = 0;		
+class Enemy extends FlxSprite
+{
+    public var speed:Float = 200;
+    var Direction:Float;
+    var MonsterType:Float;
+    public var grpMonster:FlxTypedGroup<Enemy>;
+    public function new(?X:Float = 0, ?Y:Float = 0, ?D:Float = 0, ?S:FlxTypedGroup<Enemy>, ?M:Float = 0)
+    {
+        super(X, Y);
+        Direction = D;
+        MonsterType = M;
+        if (M == 1) {
+            loadGraphic("assets/images/monster_1_fin.png", true);
+        }
+        if (M == 2) {
+            loadGraphic("assets/images/monster_2_fin.png", true);
+        }
+        if (M == 3 ) {
+            loadGraphic("assets/images/monster_3_fin.png", true);
+        }
+         
+    }
+    // Code used from HaxeFlixel documentation
+    function movement():Void
+    {
+        var _ypspeed:Float = 0;
+        var _xspeed:Float = 0;      
 
-		
-	 }
-	 
-	 
-	 override public function update(elapsed:Float):Void
-	 {
-		 movement();
-		 super.update(elapsed);
-	 }
+        
+    }
+     
+     
+    override public function update(elapsed:Float):Void
+    {
+        if (MonsterType == 0) {
+            if (Direction == 0) {
+                velocity.x = -speed;
+            }
+            else if (Direction == 1) {
+                velocity.x = speed;
+            }
+            else if (Direction == 2) {
+                velocity.y = -speed;
+            }
+            else if (Direction == 3) {
+                velocity.y = speed;
+            }
+
+        }
+
+        if (MonsterType == 1) {
+            if (Direction == 0) {
+                velocity.x = -speed;
+            }
+            else if (Direction == 1) {
+                velocity.x = speed;
+            }
+            else if (Direction == 2) {
+                velocity.y = -speed;
+            }
+            else if (Direction == 3) {
+                velocity.y = speed;
+            }
+        }
+        if (MonsterType == 2) {
+            if (Direction == 0) {
+                velocity.x = -speed;
+            }
+            else if (Direction == 1) {
+                velocity.x = speed;
+            }
+            else if (Direction == 2) {
+                velocity.y = -speed;
+            }
+            else if (Direction == 3) {
+                velocity.y = speed;
+            }
+        }
+        super.update(elapsed);
+    }
  }
