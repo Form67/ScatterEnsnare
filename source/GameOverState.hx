3a6f25e3a4b4bb03f0b4451b37cc0670fc4d0cb3
Austin <jacoba4@rpi.edu>
2018-09-25 22:47:43 -0400

    Fixed Game Over Texts

--- source/GameOverState.hx
+++ source/GameOverState.hx
@@ -19,40 +19,30 @@ class GameOverState extends FlxState
 		var num = 0;
 		if (level == 1)
 		{
-			num = r.int(0, 2);
+			num = r.int(0, 1);
 			
 			if (num == 0)
 			{
-				return "Maybe you should take up a career as an NPC instead";
+				return "Despite your efforts, your career was cut tragically short.";
 			}
 			
 			if (num == 1)
 			{
-				return "The dungeons are as barren as your profits";
-			}
-			
-			if (num == 2)
-			{
-				return "Let’s just say that you did your best and leave it at that, okay?";
+				return "Well, as long as you did your best, that’s what’s really important, right?";
 			}
 		}
 		
 		if (level == 2)
 		{
-			num = r.int(0, 2);
+			num = r.int(0, 1);
 			if (num == 0)
 			{
-				return "Your monsters made a nice side quest for a b-list RPG dungeon";
+				return "Your farming skills are steadily improving.";
 			}
 			
 			if (num == 1)
 			{
-				return "The heroes had an easy time today";
-			}
-			
-			if (num == 2)
-			{
-				return "Something is always better than nothing";
+				return "Something is always better than nothing.";
 			}
 		}
 		
@@ -61,12 +51,12 @@ class GameOverState extends FlxState
 			num = r.int(0, 1);
 			if (num == 0)
 			{
-				return "In a world of monsters, you made ends meet just fine";
+				return "I’d say you made a respectable effort!";
 			}
 			
 			if (num == 1)
 			{
-				return "I’d say you made a respectable effort";
+				return "Seems like you’re starting to get the hang of this line of work!";
 			}
 		}
 		
@@ -75,18 +65,18 @@ class GameOverState extends FlxState
 			num = r.int(0, 1);
 			if (num == 0)
 			{
-				return "Solid work!  You’re starting to make a name for yourself among RPG developers who are seeking well-grown monsters";
+				return "You’re starting to make a name for yourself among RPG developers who are seeking well-grown monsters.";
 			}
 			
 			if (num == 1)
 			{
-				return "Now that’s what I call a dungeon!";
+				return "Up and coming farmers have started asking you for advice on how to run their farms.  Good work!";
 			}
 		}
 		
 		if (level == 5)
 		{
-			return "You’re living the high life!  Perhaps you’d like to settle down in some foreign kingdom when you retire?";
+			return "With monopolies across the board, you’re set for life!  Time for a world trip to any RPG location you desire.";
 		}
 		
 		return "oops!";
